282a71ccd9d55b8e20d211417d86a20bfabf3776
samarthbhargav <samarth.bhargav92@gmail.com>
2024-05-06 15:24:23 +0200

    upd

--- train_dense.py
+++ train_dense.py
@@ -61,7 +61,8 @@ if __name__ == '__main__':
                         help="number of negatives to use during training")
     parser.add_argument("--n_negatives", default=30, type=int, help="number of negatives to obtain")
     parser.add_argument("--encode_after_train", action="store_true", default=False, help="encode & run after training ")
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s')
 
     args = parser.parse_args()
     utils.set_seed(args.seed)
